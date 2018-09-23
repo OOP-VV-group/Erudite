46ecce2432ffa66c22d532f6380a3b79a07f26e1
Семен Вяцков <43330319+susimus@users.noreply.github.com>
2018-09-23 23:16:38 +0500

    Правки Main
    * Убраны лишние табуляции

--- src/chatBot/Main.java
+++ src/chatBot/Main.java
@@ -5,10 +5,10 @@ public class Main {
 	
 	public static void main(String[] args) throws Exception {
 		IOConsole.sendBotMessages(new String[] { TextGenerator.getHelp(), 
-												 "Команды: ",
-												 "    help - вывести справку",
-												 "    quit - закончить сессию",
-												 ""});	
+				"Команды: ",
+				"    help - вывести справку",
+				"    quit - закончить сессию",
+				""});	
 		while (true) {
 			var question = TextGenerator.getQuestion();
 			IOConsole.sendBotMessages(new String[] { question.getQuestion() });
@@ -25,7 +25,7 @@ public class Main {
 				IOConsole.sendBotMessages(new String[] { "Правильный ответ!" , ""});
 			else
 				IOConsole.sendBotMessages(new String[] { "Неправильный ответ! Правильный ответ: " +
-														 question.getAnswer(), ""});
+						question.getAnswer(), ""});
 		}
 		IOConsole.closeScanner();
 	}

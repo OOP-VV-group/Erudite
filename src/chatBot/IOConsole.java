1d52cb80ad1759eb2f1e2c0070dc85aa81b9eda0
Семен Вяцков <43330319+susimus@users.noreply.github.com>
2018-12-09 22:34:56 +0500

    Принципиальная переделка работы Эрудита
    * Ядро Эрудита теперь не взаимодействует с вводом/выводом напрямую
    * Небольшие изменения интерфейса IOModule
    * Переименование стратеров в сервера
    * Введение заглушки ввода/вывода VK

--- src/chatBot/IOConsole.java
+++ src/chatBot/IOConsole.java
@@ -2,24 +2,34 @@ package chatBot;
 
 import java.util.Scanner;
 
-public enum IOConsole implements IOModule{
-	INSTANCE;
-	
+public class IOConsole implements IOModule {
 	private Scanner scanner;
 	private int id = 0;
+	private static IOConsole consoleIO = null;
 	
-	public void closeIO() {
-		this.scanner.close();
+	private IOConsole() {
+		IOConsole.consoleIO = this;
 	}
 	
-	public void openIO() {
+	public static IOConsole getInstance() {
+		if (IOConsole.consoleIO == null)
+			IOConsole.consoleIO = new IOConsole();
+
+		return IOConsole.consoleIO;
+	}
+	
+	public void open() {
 		this.scanner = new Scanner(System.in);
 	}
-	
-	public int getId() {
-		return this.id;
+
+	public void close() {
+		this.scanner.close(); 
 	}
-	
+
+	public int getId() { 
+		return this.id; 
+	}
+
 	public void sendBotMessages(String[] messages) {
 		for (String message : messages)
 			System.out.println(message);	
@@ -27,6 +37,5 @@ public enum IOConsole implements IOModule{
 
 	public String[] collectUserMessages() {
 		return new String[] { this.scanner.nextLine() };
-	}
-	
+	}	
 }
--- src/starters/VkontakteStarter.java
+++
@@ -1,8 +0,0 @@
-package starters;
-
-public class VkontakteStarter {
-
-	public static void main(String[] args) {
-		throw new RuntimeException("Not implemented yet!"); 
-	}
-}

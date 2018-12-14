199f61eb4f91ea7e6fda1ec50fe469e9998b8fdf
4RandR <4randrofficial@gmail.com>
2018-12-15 04:22:19 +0500

    Подправлены тесты

--- src/tests/TextGeneratorTest.java
+++ src/tests/TextGeneratorTest.java
@@ -1,6 +1,8 @@
 package tests;
 
 import chatBot.TextGenerator;
+import junit.framework.Assert;
+
 import static org.junit.Assert.*;
 
 import java.io.IOException;
@@ -9,10 +11,13 @@ import java.io.LineNumberReader;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.regex.Pattern; 
+import java.util.Map;
+import java.util.regex.Pattern;
 
-
+import org.junit.Before;
 import org.junit.Test;
 
 public class TextGeneratorTest
@@ -27,117 +32,65 @@ public class TextGeneratorTest
 				+ "Все даты записывай в формате ДД.ММ.ГГГГ, а имена в формате ИФ");
 	} 
 	
+	private Map<String, String> questionsAndAnswers = new HashMap<String, String>();
+	private int count = 0;
+	
+	@Before
+	public void fillQuestionsAndAnswers()
+	{
+		questionsAndAnswers.put("Кому была вручена Премия «Оскар» за лучшую женскую роль второго плана в 1995 году?", "Дайан Уист");
+		questionsAndAnswers.put("Кому была вручена Премия «Оскар» за лучшую женскую роль второго плана в 2001 году?", "Марша Гей Харден");
+		questionsAndAnswers.put("Кому была вручена Премия «Оскар» за лучшую женскую роль второго плана в 2015 году?", "Патрисия Аркетт");
+		questionsAndAnswers.put("Кому была вручена Премия «Оскар» за лучшую женскую роль второго плана в 1965 году?", "Лиля Кедрова");
+		questionsAndAnswers.put("Кому была вручена Премия «Оскар» за лучшую женскую роль второго плана в 2000 году?", "Анджелина Джоли");
+		questionsAndAnswers.put("Кому была вручена Премия «Оскар» за лучшую женскую роль в 2017 году?", "Эмма Стоун");
+		questionsAndAnswers.put("Кому была вручена Премия «Оскар» за лучшую женскую роль в 1990 году?", "Джессика Тэнди");
+		questionsAndAnswers.put("Кому была вручена Премия «Оскар» за лучшую женскую роль в 1970 году?", "Мэгги Смит");
+		questionsAndAnswers.put("Кому была вручена Премия «Оскар» за лучшую женскую роль в 2000 году?", "Хилари Суонк");
+		questionsAndAnswers.put("Кому была вручена Премия «Оскар» за лучшую женскую роль в 1980 году?", "Салли Филд");
+		questionsAndAnswers.put("Кому была вручена Премия «Золотой глобус» за лучшую мужскую роль — драма в 2017 году?", "Кейси Аффлек");
+		questionsAndAnswers.put("Кому была вручена Премия «Золотой глобус» за лучшую мужскую роль — драма в 2016 году?", "Леонардо Ди Каприо");
+		questionsAndAnswers.put("Кому была вручена Премия «Золотой глобус» за лучшую мужскую роль — драма в 1986 году?", "Джон Войт");
+		questionsAndAnswers.put("Кому была вручена Премия «Золотой глобус» за лучшую мужскую роль — драма в 1970 году?", "Джон Уэйн");
+		questionsAndAnswers.put("Кому была вручена Премия «Золотой глобус» за лучшую мужскую роль — драма в 1969 году?", "Питер О’Тул");
+		questionsAndAnswers.put("Когда родился(ась) Фадеев, Александр Александрович?", "24.12.1901");
+		questionsAndAnswers.put("Когда родился(ась) Барто, Агния Львовна?", "17.02.1906");
+		questionsAndAnswers.put("Когда родился(ась) Пушкин, Александр Сергеевич?", "06.06.1799");
+		questionsAndAnswers.put("Когда родился(ась) Сумароков, Александр Петрович?", "25.11.1717");
+		questionsAndAnswers.put("Когда родился(ась) Когда родился(ась) Гоголь, Николай Васильевич?", "01.04.1809");
+		questionsAndAnswers.put("Кому была вручена Премия «Оскар» за лучшую мужскую роль в 2018 году?", "Гэри Олдмен");
+		questionsAndAnswers.put("Кому была вручена Премия «Оскар» за лучшую мужскую роль в 1988 году?", "Майкл Дуглас");
+		questionsAndAnswers.put("Кому была вручена Премия «Оскар» за лучшую мужскую роль в 1970 году?", "Джон Уэйн");
+		questionsAndAnswers.put("Кому была вручена Премия «Оскар» за лучшую мужскую роль в 1960 году?", "Чарлтон Хестон");
+		questionsAndAnswers.put("Кому была вручена Премия «Оскар» за лучшую мужскую роль в 1953 году?", "Гэри Купер");
+		questionsAndAnswers.put("Кому была вручена Премия «Оскар» за лучшую мужскую роль второго плана в 2009 году?", "Хит Леджер");
+		questionsAndAnswers.put("Кому была вручена Премия «Оскар» за лучшую мужскую роль второго плана в 2000 году?", "Майкл Кейн");
+		questionsAndAnswers.put("Кому была вручена Премия «Оскар» за лучшую мужскую роль второго плана в 1981 году?", "Тимоти Хаттон");
+		questionsAndAnswers.put("Кому была вручена Премия «Оскар» за лучшую мужскую роль второго плана в 1975 году?", "Роберт Де Ниро");
+		questionsAndAnswers.put("Кому была вручена Премия «Оскар» за лучшую мужскую роль второго плана в 1960 году?", "Хью Гриффит");
+	}
+	
 	@Test
 	public void testGetQuestion()
 	{
 		var result = true;
-		try
+		while(count < questionsAndAnswers.size())
 		{
-			for(var i = 0; i < 100; i ++)
-				result = result && testGetQuestionHelper();
-		}
-		catch(IOException IO)
-		{
-			assert false : "Нет подключение к сети Интернет";
-		}
-		catch(IllegalArgumentException ill)
-		{
-			assert false;
-		}
-		assertTrue(result);
-	}
-	
-	private boolean testGetQuestionHelper() throws IOException, IllegalArgumentException
-	{
-		var mediaWikiPage = "https://ru.wikipedia.org/w/api.php?format=xml&action=query&"
-				+ "prop=revisions&rvprop=content&titles=";
-		var answerQuestion = tg.getQuestion(0);
-		var question = answerQuestion.getQuestion();
-		var answer = answerQuestion.getAnswer();
-		if (question.contains("Премия"))
-		{
-			var result =  workWithAward(question, mediaWikiPage);
-			return result.equals(answer);
-		}
-		else if (question.contains("Когда родился(ась) "))
-		{
-			var result = workWithPerson(question, mediaWikiPage);
-			return result.equals(answer);
-		}
-		throw new IllegalArgumentException();
-	}
-	
-	private String workWithAward(String question, String mediaWikiPage) throws IOException, IllegalArgumentException
-	{
-		var award = question.split(" в ")[0].replaceAll("Кому была вручена ", "").replaceAll(" ", "_");
-		var year = question.split(" в ")[1].replaceAll(" году?", "");
-		year = year.substring(0, year.length() - 1);
-		var page = mediaWikiPage + award;
-		var url = new URL(page);
-		var reader = new LineNumberReader(new InputStreamReader(url.openStream(), "UTF-8"));
-		var line = reader.readLine();
-		var rex = Pattern.compile("\\[\\[[а-я А-Я]+? \\([кино]*?премия, " + year + "\\)(\\|)[0-9]+?-я[\\w\\W]*?\\]\\]");
-		while(line != null)
-		{
-			var match = rex.matcher(line);
-			if (match.find())
+			try
 			{
-				var newPattern = "\\[\\[([[А-Я а-яЁё] \\.’\\,\\(\\)\\|-]+?)\\]\\]";
-				var newRex = Pattern.compile(newPattern);
-				while (line!=null)
-				{	
-					var newMatch = newRex.matcher(line);
-					if (newMatch.find())
+				var questionAnswer = tg.getQuestion(0);
+				for(var question : questionsAndAnswers.keySet())
+					if (questionAnswer.getQuestion().equals(question))
 					{
-						var index = newMatch.group(1).indexOf('|');
-						return newMatch.group(1).substring(index + 1);
+						//System.out.println(questionAnswer.getQuestion());
+						//System.out.println(questionAnswer.getAnswer());
+						result = result && questionAnswer.getAnswer().equals(questionsAndAnswers.get(question));
+						//System.out.println(result);
+						count++;
 					}
-					line = reader.readLine();
-				}
 			}
-			line = reader.readLine();
+			catch(IOException IO) {System.exit(-1);}
 		}
-		throw new IllegalArgumentException();
-	}
-	
-	private String workWithPerson(String question, String mediaWikiPage) throws IOException, IllegalArgumentException
-	{
-		var person = question.substring(19);
-    	person = person.substring(0, person.length() - 1).replaceAll(" ", "_");
-		var page = mediaWikiPage + person;
-		var url = new URL(page);
-		var reader = new LineNumberReader(new InputStreamReader(url.openStream(), "UTF-8"));
-		var line = reader.readLine();
-		var rex = Pattern.compile("([0-9]{1,2})\\|([а-я]+?)\\|([0-9]{4})");
-		while(line != null)
-		{
-			var match = rex.matcher(line);
-			if (match.find())
-				return getFormat(match.group(1) + " " + match.group(2) + " " + match.group(3));
-			line = reader.readLine();
-		}
-		throw new IllegalArgumentException();
-	}
-	
-	private String getFormat(String information)
-	{
-		String[] text = information.split(" ");
-		String mounth = "";
-		if (text[1].compareTo("января") == 0) mounth = ".01.";
-		else if (text[1].compareTo("февраля") == 0) mounth = ".02.";
-		else if (text[1].compareTo("марта") == 0) mounth = ".03.";
-		else if (text[1].compareTo("апреля") == 0) mounth = ".04.";
-		else if (text[1].compareTo("мая") == 0) mounth = ".05.";
-		else if (text[1].compareTo("июня") == 0) mounth = ".06.";
-		else if (text[1].compareTo("июля") == 0) mounth = ".07.";
-		else if (text[1].compareTo("августа") == 0) mounth = ".08.";
-		else if (text[1].compareTo("сентября") == 0) mounth = ".09.";
-		else if (text[1].compareTo("октября") == 0) mounth = ".10.";
-		else if (text[1].compareTo("ноября") == 0) mounth = ".11.";
-		else mounth = ".12.";
-		if (text[0].length() == 1)
-			text[0] = "0" + text[0];
-		String formatedinformation = text[0] + mounth + text[2];
-		return formatedinformation;
+		assert result;
 	}
 }
